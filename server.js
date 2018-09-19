d1bda1eaa7748c623b64194214db87793075b4a8
Lina Lockheart <angelinalockheart@gmail.com>
2018-09-19 12:53:54 -0400

    articles populating

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var exphbs = require('express-handlebars');
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var ObjectId = require("mongoose").Types.ObjectId;  //Am I using this here? from Ed's video
 var request = require("request");
 var cheerio = require("cheerio");
 
@@ -12,6 +13,8 @@ var db = require("./models");
 
 var app = express();
 var router = express.Router(); //FTV
+// require("./config/routes")(router); //FTV
+// require("./config/routes"); 
 
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,8 +23,6 @@ app.use(router); //FTV
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
-// mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
@@ -40,7 +41,7 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, function(err){
 });
 
 
-// Routes
+/////////// Routes
 
 app.get("/", function(req, res) {
   db.Article.find({}).then(function(dbArticles){
@@ -83,6 +84,8 @@ app.get("/scrape", function(req, res) {
   })
     res.send("Scrape Complete");
   });
+
+
 
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
