f278bfc0597b88cbe32bab97021fcee1688b03ac
Lina Lockheart <angelinalockheart@gmail.com>
2018-09-19 10:43:19 -0400

    restructure assets folder

--- server.js
+++ server.js
@@ -23,9 +23,14 @@ mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true }
 
 // Routes
 
-app.get('/', function(req, res) {
-  res.redirect("/articles");
-});
+app.get("/", function(req, res) {
+  db.Article.find({}).then(function(dbArticles){
+    res.render("index", { dbArticles: dbArticles });
+    console.log("dbArticles" + dbArticles);
+  }).catch(function(err) {
+    console.log(err);
+  })
+})
 
 app.get("/scrape", function(req, res) {
   // CHANGE WEBSITE
@@ -36,7 +41,7 @@ app.get("/scrape", function(req, res) {
     $(".title").each(function(i, element) {
       // Save an empty result object
       var result = {};
-      console.log(result);
+      console.log("result" + result);
 
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
@@ -53,12 +58,10 @@ app.get("/scrape", function(req, res) {
           console.log(dbArticle);
         })
         .catch(function(err) {
-          // If an error occurred, send it to the client
           return res.json(err);
         });
     })
   })
-    // If we were able to successfully scrape and save an Article, send a message to the client
     res.send("Scrape Complete");
   });
 
@@ -67,11 +70,9 @@ app.get("/articles", function(req, res) {
   // Grab every document in the Articles collection
   db.Article.find({})
     .then(function(dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
@@ -80,10 +81,8 @@ app.get("/articles", function(req, res) {
 app.get("/articles/:id", function(req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
     .populate("note")
     .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
@@ -93,16 +92,11 @@ app.get("/articles/:id", function(req, res) {
 
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
     .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
     .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
